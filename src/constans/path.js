98a1fe42474ce815b18dd1c194f22a9613b3d54e
Evgeniy-Popov-98 <evgeniy6183@gmail.com>
2024-05-19 15:18:36 +0300

    first

---
+++ src/constans/path.js
@@ -0,0 +1,5 @@
+import path from "node:path";
+
+const PATH_URL = path.join(process.cwd(), "src", "db", "db.json");
+
+export default PATH_URL;
---
+++ src/scripts/createProducts.js
@@ -0,0 +1,23 @@
+import PATH_URL from "../constans/path.js";
+import fs from "node:fs/promises";
+import { createFakeProduct } from "../utils/createFakeProduct.js";
+
+const createProducts = async (amount) => {
+  try {
+    const dataPoducts = await fs.readFile(PATH_URL);
+
+    const data = JSON.parse(dataPoducts);
+
+    for (let i = 0; i < amount; i = +1) {
+      data.push(createFakeProduct());
+    }
+
+    const newData = JSON.stringify(data, null, 2);
+
+    await fs.writeFile(PATH_URL, newData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+createProducts(3);
