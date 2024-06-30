2e7c4dc2387145c8cf7447d4729102ad32872e69
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-06-30 15:33:56 +0300

    update register user

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,12 +1,17 @@
-import createHttpError from 'http-errors';
-import { createUser, findUserByEmail } from '../services/users.js';
+import createHttpError from "http-errors";
+import { createUser, findUserByEmail } from "../services/users.js";
 
 export const registerUserController = async (req, res, next) => {
   const isExisting = await findUserByEmail(req.body.email);
 
   if (isExisting) {
-    throw createHttpError(409, 'User with such email already exists.');
+    throw createHttpError(409, "User with such email already exists.");
   }
 
   const user = await createUser(req.body);
+
+  res.status(201).json({
+    user: { name: user.name, email: user.email },
+    token: user.token,
+  });
 };
